9ee41a2f2ed5fdd99004ae10b90fe8393ca4a0ec
TuongNgo-Saritasa <tuong.ngo@saritasa.com>
2024-01-02 14:21:51 +0700

    Intial project

---
+++ Asdice/Assets/Scripts/Destructible.cs
@@ -0,0 +1,110 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] 
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private MeshRenderer objectRenderer;
+
+    [SerializeField]
+    private Collider objectCollider;
+
+    [SerializeField]
+    private Rigidbody body;
+
+    [SerializeField]
+    private GameObject brokenPrefab;
+
+    [SerializeField]
+    private AudioClip destructionClip;
+
+    [SerializeField]
+    private float force = 1000f;
+
+    [SerializeField]
+    private float radius = 5f;
+
+    [SerializeField]
+    private float fadeSpeed = 0.25f;
+
+    [SerializeField]
+    private float fadeDelay = 2f;
+
+    [SerializeField]
+    private float PieceSleepDelay = 0.5f;
+    
+    //Explode
+    public void SelfExplode()
+    {
+        Destroy(body);
+
+        objectRenderer.enabled = false;
+        objectCollider.enabled = false;
+        audioSource.PlayOneShot(destructionClip);
+
+        GameObject brokenObject = Instantiate(brokenPrefab, transform.position, transform.rotation); ;
+        Rigidbody[] rigidbodies = brokenObject.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody childBody in rigidbodies)
+        {
+            childBody.velocity = body.velocity;
+            childBody.AddExplosionForce(force, transform.position, radius);
+        }
+        FadeOutRigidBodies(rigidbodies).Forget();
+    }
+
+    private async UniTask FadeOutRigidBodies(Rigidbody[] rigidbodies)
+    {
+        await UniTask.Delay(TimeSpan.FromSeconds(PieceSleepDelay), ignoreTimeScale: false);
+        int activeRigidBodies = rigidbodies.Length;
+        while (activeRigidBodies > 0)
+        {
+            await UniTask.Yield();
+            foreach (Rigidbody childBody in rigidbodies)
+            {
+                if (childBody.IsSleeping())
+                {
+                    activeRigidBodies--;
+                }
+            }
+        }
+        await UniTask.Delay(TimeSpan.FromSeconds(fadeDelay), ignoreTimeScale: false);
+        float time = 0f;
+        Renderer[] renderers = Array.ConvertAll(rigidbodies, GetRenderersFromRigidBodies);
+        foreach (Rigidbody childBody in rigidbodies)
+        {
+            Destroy(childBody.GetComponent<Collider>());
+            Destroy(childBody);
+        }
+
+        while (time < 1)
+        {
+            float step = Time.deltaTime * fadeSpeed;
+            foreach(Renderer renderer in renderers)
+            {
+                renderer.transform.Translate(Vector3.down * (step / renderer.bounds.size.y), Space.World);
+            }
+            time += step;
+            await UniTask.Yield();
+        }
+
+        foreach(Renderer renderer in renderers)
+        {
+            Destroy(renderer.gameObject);
+        }
+
+        Destroy(gameObject);
+    }
+
+
+    private Renderer GetRenderersFromRigidBodies(Rigidbody rid)
+    {
+        return rid.GetComponent<Renderer>();
+    }
+}
---
+++ Asdice/Assets/Scripts/DiceAnimation.cs
@@ -0,0 +1,101 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class DiceAnimation : MonoBehaviour
+{
+    public float rotationSpeed = 30f; // Degrees per second
+
+    [SerializeField]
+    private float initialAngular = 10;
+
+    [SerializeField]
+    private float timeToRotate = 5f;
+
+    [SerializeField]
+    private float firstPhaseSpeedModifier = 0.8f;
+
+    [SerializeField]
+    private float secondPhaseSpeedModifier = 0.9f;
+
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private Destructible destructible;
+
+    private CancellationTokenSource cancellationTokenSource;
+    private float currentTime = 0f;
+    private float firstPhaseTime = 0;
+    private float secondPhaseTime = 0;
+    private bool isFirstPhase = false;
+    private bool isSecondPhase = false;
+
+    //Event for long press
+    public event Action OnDiceLongPress;
+
+    private void Awake()
+    {
+        firstPhaseTime = timeToRotate / 5f;
+        secondPhaseTime = timeToRotate / 2f;
+    }
+
+    /// <summary>
+    /// On Pressed Dice.
+    /// </summary>
+    public void OnPressedDice()
+    {
+        cancellationTokenSource = new CancellationTokenSource();
+        rb.maxAngularVelocity = initialAngular;
+        rb.AddTorque(RandomVector3() * rotationSpeed, ForceMode.Impulse);
+        UpdateTimer(cancellationTokenSource.Token).Forget();
+    }
+
+    private async UniTaskVoid UpdateTimer(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            currentTime += Time.deltaTime;
+            await UniTask.Yield();
+            if (currentTime >= timeToRotate)
+            {
+                OnDeselectDice();
+                destructible.SelfExplode();
+            }
+            else if (currentTime >= secondPhaseTime && !isSecondPhase)
+            {
+                rb.maxAngularVelocity = secondPhaseSpeedModifier;
+                rb.AddTorque(rotationSpeed * secondPhaseSpeedModifier * RandomVector3(), ForceMode.Impulse);
+                isSecondPhase = true;
+            }
+            else if (currentTime >= firstPhaseTime && !isFirstPhase)
+            {
+                rb.maxAngularVelocity = firstPhaseSpeedModifier;
+                rb.AddTorque(firstPhaseSpeedModifier * rotationSpeed * RandomVector3(), ForceMode.Impulse);
+                isFirstPhase = true;
+            }
+        }
+    }
+
+    private Vector3 RandomVector3()
+    {
+        return new Vector3(UnityEngine.Random.Range(0f, 360f), UnityEngine.Random.Range(0f, 360f), UnityEngine.Random.Range(0f, 360f));
+    }
+
+    /// <summary>
+    /// On Deselected Dice.
+    /// </summary>
+    public void OnDeselectDice()
+    {
+        cancellationTokenSource.Cancel();
+        currentTime = 0f;
+        rb.angularVelocity = Vector3.zero;
+        isSecondPhase = false;
+        isFirstPhase = false;
+    }
+}
